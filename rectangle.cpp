074ca3ab36ab5bf58c811db3d1e68dbe41b1de7a
zosia-zosia <zosiasz151@gmail.com>
2025-11-17 10:11:53 +0000

    76

--- rectangle.cpp
+++ rectangle.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+using namespace std;
 
 class Rectangle {
+private:
+    double width;
+    double height;
 
+public:
+    Rectangle(double w, double h)
+        : width(w), height(h) {}
+
+    void setWidth(double w) {
+        width = w;
+    }
+
+    void setHeight(double h) {
+        height = h;
+    }
+
+    double getWidth() const {
+        return width;
+    }
+
+    double getHeight() const {
+        return height;
+    }
+
+    double area() const {
+        return width * height;
+    }
+
+    double perimeter() const {
+        return 2 * (width + height);
+    }
+
+    void display() const {
+        cout << "Rectangle " << width << " x " << height << endl;
+        cout << "Area " << area() << endl;
+        cout << "Perimeter " << perimeter() << endl;
+    }
 };
